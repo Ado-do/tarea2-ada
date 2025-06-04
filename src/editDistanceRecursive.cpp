d1e8ec250bbf763f92091b543318fb07c3f1bb69
Ado-do <alonbustos2021@udec.cl>
2025-06-04 17:13:35 -0400

    update editDistanceRecursive.cpp

--- src/editDistanceRecursive.cpp
+++ src/editDistanceRecursive.cpp
@@ -1,28 +1,21 @@
-#include <iomanip>
 #include <iostream>
 #include <string>
 
 using namespace std;
 
-int RecursiveStep(string &s1, string &s2, int n, int m)
+int editDistanceRecursive(string &s1, string &s2, int i, int j)
 {
-    // Caso sub-secuencia no encontrada
-    if (n == 0 || m == 0) return 0;
-
-    // Caso coinciden al final (restar a los dos)
-    if (s1[n-1] == s2[m-1]) {
-        return RecursiveStep(s1, s2, n-1, m-1) + 1;
+    if (i == 0 || j == 0) {
+        if (i == 0) return j; // s1 recorrida completamente, se insertan los j caracteres restantes de s2
+        if (j == 0) return i; // s2 recorrida completamente, se borran los i caracteres restantes de s1
     }
 
-    // Caso no coinciden al final (se resta a la mas conveniente)
-    return max(RecursiveStep(s1, s2, n-1, m),
-               RecursiveStep(s1, s2, n, m-1));
-}
-
-int LCSLength(string &s1, string &s2)
-{
-    int n = s1.length(), m = s2.length();
-    return RecursiveStep(s1, s2, n, m);
+    if (s1[i] == s2[j])
+        // s1 y s2 coinciden, no se necesita una operacion
+        return editDistanceRecursive(s1, s2, i-1, j-1);
+    else
+        // s1 y s2 no coinciden, se elige la mejor opcion entre eliminar o insertar un caracter
+        return 1 + min(editDistanceRecursive(s1, s2, i-1, j), editDistanceRecursive(s1, s2, i, j-1));
 }
 
 int main()
@@ -31,21 +24,12 @@ int main()
     getline(cin, s1);
     getline(cin, s2);
 
-    // Tamaño de la Longest Common Subsequence
-    // (Subsequence de una string: es la secuencia de caracteres de una string,
-    // no necesariamente continua, que mantiene su orden relativo respecto a la
-    // string original (ex. "Alonso" -> "Aoso")) en este caso buscamos la mas
-    // larga en común entre las strings
-    int lcs_length = LCSLength(s1, s2);
-
-    int deletes = s1.length() - lcs_length;
-    int inserts = s2.length() - lcs_length;
-    int total = deletes + inserts;
-
-    cout << "EditDistance entre\n"
-        << "s1 (" << setw(2) << s1.length() << ") =\n\t\"" << s1 << "\"\n"
-        << "s2 (" << setw(2) << s2.length() << ") =\n\t\"" << s2 << "\"\n"
-        << "es\n\t" << total << " (deletes = " << deletes << ", inserts = " << inserts << ")\n";
+    cout << "s1 =\n"
+         << "\t\"" << s1 << "\"\n"
+         << "s2 =\n"
+         << "\t\"" << s2 << "\"\n"
+         << "editDistanceRecursive(s1, s2) = \n"
+         << '\t' << editDistanceRecursive(s1, s2, s1.length() - 1, s2.length() - 1) << '\n';
 
     return 0;
 }
