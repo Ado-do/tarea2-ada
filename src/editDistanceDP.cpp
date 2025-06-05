595cabb845f8562887f6f77787c6d336f40d1316
Constanza Cristinich <109244120+krsconnie@users.noreply.github.com>
2025-06-05 04:16:55 -0400

    Versiones pseudoterminadas, falta agregar comentarios decentes y incluir lo de calcular el espacio

--- src/editDistanceDP.cpp
+++ src/editDistanceDP.cpp
@@ -1,6 +1,59 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <algorithm>
+
+
+using namespace std;
+
+int editDistanceDP(const string& s1, const string& s2) {
+    int n = s1.length(), m = s2.length();
+    vector<vector<int>> M(n + 1, vector<int>(m + 1));
+
+    // convertir S1[0...n] a cadena vacía mediante función delete
+    for (int i = 0; i <= n; ++i) M[i][0] = i;
+
+    // convertir de "" a S2[0...m] mediante función insert
+    for (int j = 0; j <= m; ++j) M[0][j] = j;
+
+    // convertir de s1[...] a s2[...] mediante inserciones y borrados
+    for (int i = 1; i <= n; ++i) {
+        for (int j = 1; j <= m; ++j) {
+            if (s1[i - 1] == s2[j - 1]) {  // Si un caracter de s1 coincide con otro de s2 hay 3 casos 
+                M[i][j] = min({
+                			M[i - 1][j] + 1,    // Se borra de s1 para que sea s2
+                            M[i][j - 1] + 1,    // Se inserta en s2 para que se convierta en s1
+                            M[i - 1][j - 1]});  // Son iguales en la misma posición por lo que se mantiene
+            } else { 						
+            							// Si no coinciden, hay 2 casos
+                M[i][j] = min(
+                			M[i - 1][j] + 1,      // Se borra de s1 para transformarlo a s2
+                            M[i][j - 1] + 1);      // Se inserta en s2 para transformarlo a s1
+            }
+        }
+    }
+
+    return M[n][m];
+}
 
 int main() {
-    std::cout << "editDistanceDP\n";
+    
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+    
+    auto start = std::chrono::high_resolution_clock::now();
+
+    int dist = editDistanceDP(s1, s2);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+
+    cout << "Distancia de edicion optimizada:" << dist << "\nTiempo utilizado:" << duration.count() <<
+    "\nEspacio utilizado:" << endl;
+
     return 0;
 }
+
--- src/editDistanceDPOptimized.cpp
+++ src/editDistanceDPOptimized.cpp
@@ -1,6 +1,64 @@
 #include <iostream>
+#include <vector>
+#include <string>
+#include <algorithm>
+#include <chrono>
+
+using namespace std;
+
+int editDistanceDPOptimized(const string& s1, const string& s2) {
+    int n = s1.length(), m = s2.length();
+
+    // Siempre trabajamos con la menor cadena como fila para ahorrar espacio (menos columnas)
+    if (m < n) return editDistanceDPOptimized(s2, s1); // intercambiamos
+
+    vector<int> anterior(m + 1), actual(m + 1);
+
+    // convertir "" en s2[..] mediante inserciones
+    for (int j = 0; j <= m; ++j) anterior[j] = j;
+
+    for (int i = 1; i <= n; ++i) {
+        actual[0] = i;  // borrar todos los de s1[..] para volverse ""
+
+        for (int j = 1; j <= m; ++j) { // Mismos casos que en la versión no optimizada (cuando el caracter coincide o no)
+            if (s1[i - 1] == s2[j - 1]) {
+
+                actual[j] = min({anterior[j] + 1,       
+                               actual[j - 1] + 1,   
+                               anterior[j - 1]});   
+
+            } else {
+
+                actual[j] = min(anterior[j] + 1,        
+                              actual[j - 1] + 1);   
+            }
+        }
+
+        // Intercambiar filas: la actual se vuelve la anterior
+        swap(anterior, actual);
+    }
+
+    int resultado = anterior[m];
+
+    return resultado;
+}
 
 int main() {
-    std::cout << "editDistanceDPOptimized\n";
+
+    string s1, s2;
+    getline(cin, s1);
+    getline(cin, s2);
+
+	auto start = std::chrono::high_resolution_clock::now();
+    
+    int dist = editDistanceDPOptimized(s1, s2);
+
+    auto end = std::chrono::high_resolution_clock::now();
+    
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start);
+    
+    cout << "Distancia de edicion optimizada:" << dist << "\nTiempo utilizado:" << duration.count() <<
+    "\nEspacio utilizado:" << endl;
+
     return 0;
 }
