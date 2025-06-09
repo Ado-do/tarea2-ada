12858718d9caf166dfb5a2f61f1fd9306579b342
Ado-do <alonbustos2021@udec.cl>
2025-06-09 17:41:39 -0400

    minor change

--- medir_graficar_todo.sh
+++ medir_graficar_todo.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-./scripts/medir_memoria.sh
-./scripts/medir_tiempo.sh
+bash ./scripts/medir_memoria.sh
+bash ./scripts/medir_tiempo.sh
 python ./scripts/graficar_memoria.py
 python ./scripts/graficar_tiempo.py
